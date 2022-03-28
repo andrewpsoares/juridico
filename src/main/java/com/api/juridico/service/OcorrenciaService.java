a7ec75e1ec7ab5f278ca21cbb5ff6acb4a07f4c4
Andrew Soares <andrewsoares1@gmail.com>
2022-03-28 15:27:38 -0300

    backend Ocorrencia

--- src/main/java/com/api/juridico/service/OcorrenciaService.java
+++ src/main/java/com/api/juridico/service/OcorrenciaService.java
@@ -3,19 +3,40 @@ package com.api.juridico.service;
 import com.api.juridico.config.ObjectMapperConfig;
 import com.api.juridico.domain.model.Ocorrencia;
 import com.api.juridico.domain.model.Processo;
+import com.api.juridico.domain.repository.ProcessoRepository;
 import com.api.juridico.dto.OcorrenciaDto;
 import com.api.juridico.dto.ProcessoDto;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
 public class OcorrenciaService {
 
     private ProcessoService processoService;
+
+    @Autowired()
+    private ProcessoRepository processoRepository;
+
+    @Transactional(readOnly = true)
+    public List<OcorrenciaDto> findAll(Long idProcesso){
+        Optional<Processo> processo = processoRepository.findById(idProcesso);
+        if (processo.isPresent()) {
+            List<Ocorrencia> ocorrencia = processo.get().getOcorrecias();
+            return ObjectMapperConfig.mapAll(ocorrencia, OcorrenciaDto.class);
+        }else{
+            return new ArrayList<>();
+        }
+    }
 
     @Transactional
     public OcorrenciaDto insert(Long idProcesso, String descricao){
@@ -27,5 +48,4 @@ public class OcorrenciaService {
             return null;
         }
     }
-
 }
