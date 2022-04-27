a81dcffe9a863adfc6a65f11f19e5d28cd867873
Andrew Soares <andrewsoares1@gmail.com>
2022-04-27 07:42:55 -0300

    Swagger

--- backend/src/main/java/com/api/juridico/JuridicoApplication.java
+++ backend/src/main/java/com/api/juridico/JuridicoApplication.java
@@ -2,8 +2,16 @@ package com.api.juridico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+//@EnableSpringDataWebSupport
+//@EnableCaching
+//@EnableWebMvc
+@EnableSwagger2
 public class JuridicoApplication {
 
 	public static void main(String[] args) {
